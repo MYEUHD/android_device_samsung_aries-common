3f3df30baf82b94fec48008d85461d5ae2b087bc
MYEUHD <MYEUHD@protonmail.com>
2018-04-21 14:21:04 +0100

    aries-common: Add the toolchain path

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -23,6 +23,7 @@ TARGET_ARCH_VARIANT_CPU := cortex-a8
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a8
+KERNEL_TOOLCHAIN := "$(ANDROID_BUILD_TOP)/prebuilts/gcc/$(strip $(HOST_OS))-x86/arm/arm-eabi-4.8/bin/"
 
 TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
